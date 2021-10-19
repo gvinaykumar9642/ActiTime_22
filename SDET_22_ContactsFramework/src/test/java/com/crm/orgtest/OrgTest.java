c5097c245252940f061761811c80872ec388f00c
vinay <vinay@DESKTOP-ONQP60F>
2021-10-19 18:06:01 +0530

    commit2

--- SDET_22_ContactsFramework/src/test/java/com/crm/orgtest/OrgTest.java
+++ SDET_22_ContactsFramework/src/test/java/com/crm/orgtest/OrgTest.java
@@ -17,14 +17,16 @@ public class OrgTest {
 		System.out.println("execuite edit orgtest");
 	}
 	
-	@Test
-	public void OrgTest1()
-	{
-		System.out.println("execuite delete orgtest");
-	}
+	
 	@Test
 	public void modOrgTest1()
 	{
 		System.out.println("execuite modify orgtest");
+	}
+	
+	@Test
+	public void OrgTest1()
+	{
+		System.out.println("execuite delete orgtest");
 	}
 }
