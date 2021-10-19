d73474b2b1d0e500e9e3dbf8afd9ba00e07431f0
vinay <vinay@DESKTOP-ONQP60F>
2021-10-19 13:09:51 +0530

    commit user1

---
+++ SDET_22_ContactsFramework/src/test/java/com/crm/orgtest/OrgTest.java
@@ -0,0 +1,19 @@
+package com.crm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	
+	@Test
+	public void OrgTest()
+	{
+		System.out.println("execuite create orgtest");
+	}
+	
+	@Test
+	public void editorgTest()
+	{
+		System.out.println("execuite edit orgtest");
+	}
+}
